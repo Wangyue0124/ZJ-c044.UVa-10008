9d2e930df95f310bb98c6809e3daf7d84fc2c349
Wangyue0124 <diego960124@gmail.com>
2025-09-29 19:40:04 +0800

    Add files via upload

---
+++ c044. 10008 - What's Cryptanalysis.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cctype>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    vector<int>tal(26, 0);
+    int n, max;
+    string s;
+    cin >> n;
+    cin.ignore();
+    while (n--) {
+        getline(cin, s);
+        for (int i = 0; i < s.size(); i++) {
+            if (isalpha(s[i])) {
+                char ch = tolower(s[i]);
+                tal[ch - 'a']++;
+            }
+        }
+    }
+    max = *max_element(tal.begin(), tal.end());
+    while (max) {
+        for (int i = 0; i <= 25; i++) {
+            char ch = 'A' + i;
+            if (tal[i] == max) cout << ch << " " << max<<"\n";
+        }
+        max--;
+    }
+    return 0;
+}
+
